7a6b3f606d66b6bb122c45a8be209eaf23f168de
alvinxal <alvvxal@gmail.com>
2025-11-15 14:13:54 +0700

    feat: add zustand

---
+++ lib/store/authStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface AuthState {
+  token: string | null;
+  setToken: (token: string) => void;
+  logout: () => void;
+}
+
+export const authStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      token: null,
+      setToken: (newToken: string) => set({ token: newToken }),
+      logout: () => set({ token: null }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+);
