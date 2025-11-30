3a0dd0b570ce7e8cbb899907c80151beaf42eca2
Alvin Al <alvvxal@gmail.com>
2025-11-30 16:06:07 +0700

    feat: add item card

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -14,16 +15,42 @@ import { ProductTypes } from "@/lib/types";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { IoSearchSharp } from "react-icons/io5";
+import { Spinner } from "@/components/ui/spinner";
+import { FaStore, FaStar } from "react-icons/fa";
+
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState<string>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
 
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(true);
   const [products, setProducts] = useState<ProductTypes[]>([]);
+  const [searchProducts, setSearchProducts] = useState<string>("");
+  const debounceQuery = useDebounce(searchProducts, 300);
 
+  // fetch products
   useEffect(() => {
     const getProducts = async () => {
       try {
         const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}/products/public`
+          `${process.env.NEXT_PUBLIC_API_URL}/products/public`,
+          {
+            params: {
+              search: debounceQuery,
+            },
+          }
         );
 
         if (response) setProducts(response.data.data);
@@ -38,7 +65,7 @@ export default function Home() {
     };
 
     getProducts();
-  }, []);
+  }, [debounceQuery]);
 
   const productsNotFound = <>Produk tidak ada</>;
 
@@ -54,59 +81,80 @@ export default function Home() {
             <div className='absolute pl-3'>
               <IoSearchSharp size='1.2em' />
             </div>
-            <Input className='pl-10' placeholder='Cari bibit' />
+            <Input
+              className='pl-10'
+              placeholder='Cari bibit'
+              value={searchProducts}
+              onChange={(e) => setSearchProducts(e.target.value)}
+            />
           </div>
         </div>
+        {/* Product not found */}
         {!loading && products.length === 0 && productsNotFound}
-        <div className='grid gap-4 grid-cols-2 xl:grid-cols-4'>
-          {products.map((item) => (
-            <Card
-              key={item.id}
-              className='hover:shadow-lg transition-shadow p-0'
-            >
-              <CardHeader className='p-0'>
-                <div className='w-full h-48 border-b rounded-lg overflow-hidden relative'>
-                  {item.imageUrl ? (
-                    <Image
-                      src={item.imageUrl}
-                      alt={item.name}
-                      fill
-                      className='object-cover'
-                      priority
-                    />
-                  ) : (
-                    <div className='w-full h-full flex justify-center items-center text-xs text-gray-500'>
-                      No Image
+        {loading ? (
+          <div className='w-full flex justify-center'>
+            <Spinner />
+          </div>
+        ) : (
+          <div className='grid gap-4 grid-cols-2 xl:grid-cols-4'>
+            {products.map((item) => (
+              <Card
+                key={item.id}
+                className='hover:shadow-lg transition-shadow p-0 gap-0 min-h-[300px]'
+              >
+                <CardHeader className='p-0 h-40'>
+                  <div className='w-full h-40 border-b rounded-lg overflow-hidden relative'>
+                    {item.imageUrl ? (
+                      <Image
+                        src={item.imageUrl}
+                        alt={item.name}
+                        fill
+                        className='object-cover'
+                      />
+                    ) : (
+                      <div className='w-full h-full flex justify-center items-center text-xs text-gray-500'>
+                        No Image
+                      </div>
+                    )}
+                  </div>
+                </CardHeader>
+                <CardContent className='p-2 h-full flex flex-col justify-between '>
+                  <div>
+                    <div>
+                      <p className='mb-2 text-xs font-semibold'>
+                        Rp{item.price.toLocaleString("id-ID")}/{item.unit}
+                      </p>
                     </div>
-                  )}
-                </div>
-                <div className='px-2 space-y-1'>
-                  <CardTitle className='font-normal text-base overflow-clip'>
-                    {item.name}
-                  </CardTitle>
-                  <CardDescription className=''>
-                    {" "}
-                    <div>Varietas : {item.varieties}</div>
-                    <div>Penjual : {item.seller?.name}</div>
-                  </CardDescription>
-                </div>
-              </CardHeader>
-              <CardContent className='px-2'>
-                <div className='flex justify-between text-base'>
-                  <p>
-                    Stok : {item.stock} {item.unit}
-                  </p>
-                </div>
-                <div>
-                  {" "}
-                  <p className='text-gray-800 font-bold text-lg '>
-                    Rp{item.price.toLocaleString("id-ID")}/{item.unit}
-                  </p>
-                </div>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+                    <div className='flex items-center gap-1 mb-1 text-xs font-normal'>
+                      <FaStore />
+                      {item.seller?.name}
+                    </div>
+                    <div>
+                      <p className='text-sm font-semibold'>
+                        {item.name}{" "}
+                        <span className='text-sm font-normal'>
+                          ({item.varieties})
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className='text-xs'>
+                    <div className='flex items-center gap-1'>
+                      <FaStar />
+                      4.5
+                    </div>
+                    <div>
+                      {" "}
+                      <p>
+                        Stok : {item.stock} {item.unit}
+                      </p>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["localhost", "*.local-origin.dev"],
   images: {
     remotePatterns: [
       {
