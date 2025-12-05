216331c7ef7adcefaf553b273c3718feecebe1da
Alvin Al <alvvxal@gmail.com>
2025-12-06 06:07:29 +0700

    feat: add cart functional

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -9,65 +9,99 @@ import React from "react";
 import { FaRegTrashAlt, FaStore } from "react-icons/fa";
 
 const Cart = () => {
-  const products = useCartStore((state) => state.items);
+  const {
+    items: products,
+    updateQuantity,
+    clearCart,
+    removeItem,
+  } = useCartStore();
 
   return (
     <>
       <PublicNavbar />
       <div className='p-4'>
-        {products.map((product, index) => (
-          <Card key={index} className='flex p-0 gap-0 flex-row'>
-            {/* image */}
-            <div className='aspect-square w-1/2 overflow-hidden relative rounded-lg'>
-              {product.imageUrl ? (
-                <Image
-                  src={product.imageUrl}
-                  fill
-                  objectFit='cover'
-                  sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                  alt={product.name}
-                />
-              ) : (
-                <>No image</>
-              )}
-            </div>
-            {/* description */}
-            <div className='w-1/2 p-4 justify-between flex flex-col'>
-              <div className=''>
-                <h1 className='text-lg font-semibold'>{product.name}</h1>
-                <div className='flex items-center gap-1 text-sm'>
-                  <FaStore />
-                  <p>{product.seller?.name}</p>
+        <div className='mb-2'>
+          <Button onClick={() => clearCart()} variant='outline'>
+            Hapus Seluruh Keranjang
+          </Button>
+        </div>
+        <div>
+          {" "}
+          <div className=' grid grid-cols-1 gap-4'>
+            {products.map((product, index) => (
+              <Card key={index} className='flex p-0 gap-0 flex-row'>
+                {/* image */}
+                <div className='aspect-square w-1/2 overflow-hidden relative rounded-lg'>
+                  {product.imageUrl ? (
+                    <Image
+                      src={product.imageUrl}
+                      fill
+                      objectFit='cover'
+                      sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                      alt={product.name}
+                    />
+                  ) : (
+                    <>No image</>
+                  )}
                 </div>
-                <p className='text-sm mt-2 text-green-700 font-semibold space-x-1'>
-                  Rp{product?.price.toLocaleString("ID-id")}
-                  <span className='text-gray-500'>/{product?.unit}</span>
-                </p>
-              </div>
-              {/* qty button */}
-              <div className='flex justify-between'>
-                <div className='flex gap-1 '>
-                  <Button variant='outline' size='sm'>
-                    +
-                  </Button>
-                  <Input
-                    type='number'
-                    value={product.quantity}
-                    className='h-8 px-1 w-10 text-center'
-                  />
-                  <Button variant='outline' size='sm'>
-                    -
-                  </Button>
+                {/* description */}
+                <div className='w-1/2 p-4 justify-between flex flex-col'>
+                  <div className=''>
+                    <h1 className='text-lg font-semibold'>{product.name}</h1>
+                    <div className='flex items-center gap-1 text-sm'>
+                      <FaStore />
+                      <p>{product.seller?.name}</p>
+                    </div>
+                    <p className='text-sm mt-2 text-green-700 font-semibold space-x-1'>
+                      Rp{product?.price.toLocaleString("ID-id")}
+                      <span className='text-gray-500'>/{product?.unit}</span>
+                    </p>
+                  </div>
+                  {/* qty button */}
+                  <div className='flex justify-between'>
+                    <div className='flex gap-1 '>
+                      <Button
+                        variant='outline'
+                        size='sm'
+                        onClick={() =>
+                          updateQuantity(product.id, product.quantity + 1)
+                        }
+                      >
+                        +
+                      </Button>
+                      <Input
+                        type='number'
+                        value={product.quantity}
+                        className='h-8 px-1 w-10 text-center'
+                        onChange={(e) =>
+                          updateQuantity(product.id, Number(e.target.value))
+                        }
+                      />
+                      <Button
+                        variant='outline'
+                        size='sm'
+                        onClick={() =>
+                          updateQuantity(product.id, product.quantity - 1)
+                        }
+                      >
+                        -
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        variant='destructive'
+                        size='sm'
+                        onClick={() => removeItem(product.id)}
+                      >
+                        <FaRegTrashAlt />
+                      </Button>
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <Button variant='destructive' size='sm'>
-                    <FaRegTrashAlt />
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </Card>
-        ))}
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
